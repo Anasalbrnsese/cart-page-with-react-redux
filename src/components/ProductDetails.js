5869d24b5dd8a85fa78e439df008e14d6cd918b2
anasalbrnsese <anasalbrnsese@Hotmil.com>
2025-11-07 10:08:37 +0300

    Update Route Product Details using Link to go Shopping page

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 function ProductDetails() {
     return (
         <div className="flex justify-center items-center h-screen bg-black">
@@ -8,9 +9,9 @@ function ProductDetails() {
                 <p className="text-lg text-gray-600 dark:text-gray-300 max-w-md">
                     We're working hard to finish this page. Please check back soon!
                 </p>
-                <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-                    Go Back Home
-                </button>
+                <Link to={'/products'} className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition no-underline">
+                    Go Back Shopping
+                </Link>
             </div>
         </div>
     )
